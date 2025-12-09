1d1c01ad96f815c94c85cbd780f70a9d2ba2ca8d
PolinaBasargina <polbas05@mail.ru>
2025-12-09 21:58:01 +0700

    add request for all books

---
+++ frontend/src/app/core/services/book-data.service.ts
@@ -0,0 +1,15 @@
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiService } from './api.service';
+import { BookCard } from '../../shared/models/book.model';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class BookDataService {
+    private api = inject(ApiService);
+
+    getAllBooks(limit: number = 10, offset: number = 0): Observable<BookCard> {
+        return this.api.get<BookCard>('/books/all', { limit, offset });
+    }
+}
