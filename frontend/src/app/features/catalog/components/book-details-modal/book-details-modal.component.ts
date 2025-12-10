1540199124b99e234e54941d2f54db4d724a96bc
dariamukomel <daramukomel56549@gmail.com>
2025-12-10 13:17:05 +0700

    fix cover_path

--- frontend/src/app/features/catalog/components/book-details-modal/book-details-modal.component.ts
+++ frontend/src/app/features/catalog/components/book-details-modal/book-details-modal.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Book } from '../../../../shared/models/book.model';
+import { environment } from '../../../../../environments/environment';
 
 @Component({
     selector: 'app-book-details-modal',
@@ -11,6 +12,8 @@ import { Book } from '../../../../shared/models/book.model';
 export class BookDetailsModalComponent {
     @Input({ required: true }) book: Book | null = null;
     @Output() close = new EventEmitter<void>();
+
+    protected readonly apiUrl = environment.apiUrl;
 
     closeModal() {
         this.close.emit();
