dd1352b0a042367507c8d72b5222b7aa3b6abb45
PolinaBasargina <polbas05@mail.ru>
2025-12-17 00:45:58 +0700

    add division for search results

--- frontend/src/app/features/catalog/components/search-results/search-results.component.ts
+++ frontend/src/app/features/catalog/components/search-results/search-results.component.ts
@@ -1,5 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+    Component,
+    EventEmitter,
+    Input,
+    OnChanges,
+    Output,
+    SimpleChanges,
+} from '@angular/core';
 import { BookCardComponent } from '../book-card/book-card.component';
 import { SearchHit } from '../../../../shared/models/book.model';
 
@@ -9,10 +16,26 @@ import { SearchHit } from '../../../../shared/models/book.model';
     templateUrl: './search-results.component.html',
     styleUrl: './search-results.component.scss',
 })
-export class SearchResultsComponent {
+export class SearchResultsComponent implements OnChanges {
     @Input() hits: SearchHit[] = [];
     @Input() total: number = 0;
     @Output() detailsClick = new EventEmitter<number>();
+
+    metaHits: SearchHit[] = [];
+    quoteHits: SearchHit[] = [];
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes['hits'] && this.hits) {
+            this.splitHits();
+        }
+    }
+
+    private splitHits() {
+        this.metaHits = this.hits.filter((h) => h.match_type === 'meta');
+        this.quoteHits = this.hits.filter(
+            (h) => h.match_type === 'quote' || h.match_type === undefined
+        );
+    }
 
     onDetails(id: number) {
         this.detailsClick.emit(id);
